7d761c0b85975f98ce2bbdd355164ccb06cd5536
Huw Jenkins <huw.jenkins@york.ac.uk>
2026-04-28 15:34:42 +0100

    Add script to calculate mask volumes

---
+++ calc_vol.py
@@ -0,0 +1,61 @@
+import glob
+import json
+import mrcfile
+import numpy as np
+
+reject = [
+'EMD-48782', # emd_48782_msk_1.map range: 0.0000 - 34.8728
+'EMD-63643', # "The validation analysis for this entry have not been generated yet."
+'EMD-76333', # "The validation analysis for this entry have not been generated yet."
+'EMD-72283', # "The validation analysis for this entry have not been generated yet."
+'EMD-76334', # "The validation analysis for this entry have not been generated yet"
+'EMD-55890', # "The validation analysis for this entry have not been generated yet."
+'EMD-65613', # "An unexpected error occurred when attempting to display the validation data for this entry."
+'EMD-63947', # emd_63947_msk_1.map is not a mask (range = -0.2242 - 0.4311)
+'EMD-63853', # emd_63853_msk_1.map is not a mask (range = -0.3191 - 0.5647)
+'EMD-65077', # emd_65077_msk_1.map range: 0.0000 - 26.9748
+'EMD-53847', # emd_53847_msk_1.map 1.44 A/px vs reconstruction at 0.72 A/px
+'EMD-64391', # Mask voxels all 1.000
+'EMD-51161', # "The validation analysis for this entry have not been generated yet."
+'EMD-75259', # emd_75259_msk_1.map range: 0.0000 - 0.8821
+'EMD-75258', # emd_75258_msk_1.map range: 0.0000 - 0.8889
+'EMD-75265', # No FSC validation.
+'EMD-52400', # emd_52400_msk_1.map 1 A/px vs reconstruction at 1.4 A/px
+]
+
+def volume(mask_file):
+  with mrcfile.open(mask_file) as f:
+    d = f.data
+    if d.min() != 0 or d.max() != 1:
+      print(f'Warning! {mask_file} range: {d.min():0.4f} - {d.max():0.4f}')
+    apix = f.voxel_size.x
+    summask = (d > 0.5).sum()
+    volume = summask*apix**3
+    return volume, apix
+
+def calc_vol():
+  results = []
+  filtered_entries = [e for e in entries if e not in reject]
+  for id in filtered_entries:
+    n = id.split('-')[1]
+    if len(glob.glob('emd_%s_msk_*.map.gz' % n)) > 1:
+      print(f'Skipping {id} as > 1 masks deposited')
+    else:
+      d_mask = 'emd_%s_msk_1.map.gz' % n
+      emdb_mask = 'emd_%s.map_mask.mrc.gz' % n
+      print(f'Calculating results for {id} using {d_mask} and {emdb_mask}...')
+      d_volume, d_apix = volume(d_mask)
+      emdb_volume, emdb_apix = volume(emdb_mask)
+      assert d_apix == emdb_apix
+      print(f'...done. Ratio: {emdb_volume/d_volume:0.2f}')
+      results.append({'id':id, 'depositor_mask_volume':d_volume, 'emdb_mask_volume':emdb_volume})
+
+  for r in sorted(results, key=lambda x: x['emdb_mask_volume']/x['depositor_mask_volume']):
+    print(f'{r['id']} ratio:{r['emdb_mask_volume']/r['depositor_mask_volume']:0.2f}')
+  with open('results_2026-04-27.json', 'w') as f:
+    json.dump(results, f)
+
+if __name__ == '__main__':
+  with open('entries_2026-04-27.json') as f:
+    entries = json.load(f)
+  calc_vol()
