885cfb3abf8b6d45cd432b0a980a096919975b8f
Huw Jenkins <huw.jenkins@york.ac.uk>
2026-05-01 00:37:56 +0100

    Plot histograms of results

--- make_plot.py
+++ make_plot.py
@@ -1,5 +1,4 @@
 import json
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 def plot():
@@ -7,16 +6,16 @@ def plot():
     results = json.load(f)
   for r in sorted(results, key=lambda x: x['emdb_mask_volume']/x['depositor_mask_volume']):
     print(f'{r['id']} ratio:{r['emdb_mask_volume']/r['depositor_mask_volume']:0.2f}')
-  a = [r['emdb_mask_volume']/r['depositor_mask_volume'] for r in results]
-  fig, ax = plt.subplots()
-  sns.stripplot(data=a, orient='h', size=4, jitter=0.25)
-  ax.xaxis.grid(which='both')
-  ax.set_xlabel('Volume ratio EMDB mask:Depositors mask')
-  ax.set_xlim(0,50)
-  ax.xaxis.set_ticks(range(0,51,5))
-  fig.savefig('results_2026-04-27.pdf', format='pdf')
-  ax.set_xlim(0,20)
-  ax.xaxis.set_ticks(range(0,21,1))
-  fig.savefig('results_2026-04-27_zoom.pdf', format='pdf')
+  x = [r['emdb_mask_volume']/r['map_volume'] for r in results]
+  y = [r['depositor_mask_volume']/r['map_volume'] for r in results]
+  for d, mask in [[x,'EMDB'], [y, 'Depositor']]:
+    fig, ax = plt.subplots()
+    ax.hist(d, bins=100, range=(0,200))
+    ax.set_xlim(0,200)
+    ax.set_xlabel(f'Volume ratio {mask} mask:Primary map')
+    ax.set_ylabel('Count')
+    plt.title(f'{mask} mask volume compared to Primary map volume')
+    fig.savefig(f'results_{mask}_2026-04-27.pdf', format='pdf')
+
 if __name__ == '__main__':
   plot()
